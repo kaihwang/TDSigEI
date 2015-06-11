8226e0d8cf06a4a556d21432780c491738657cc2
Kai Hwang <kai.hwang@gmail.com>
2015-06-11 15:07:24 -0700

    update for real subject

--- sge_submit_functional.sh
+++ sge_submit_functional.sh
@@ -3,19 +3,19 @@
 #for SGE jobs
 
 #mkdir tmp;
-WD='/home/despoB/kaihwang/TDSigEI'
+WD='/home/despoB/TRSEPPI/TDSigEI'
 SCRIPTS='/home/despoB/kaihwang/bin/TDSigEI'
 
 cd ${WD}
 
-for Subject in $(ls -d *); do
+for Subject in 503; do
 	
 	#if [ ! -e "${WD}/${Subject}/MPRAGE/${Subject}_MNI_final.nii.gz" ]; then
 	#	sed "s/s in 107/s in ${Subject}/g" < ${SCRIPT}/preproc_mprage.sh > ${SCRIPT}/tmp/mprage_${Subject}.sh
 	#	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp ${SCRIPT}/tmp/mprage_${Subject}.sh  #-l mem_free=5G
 	#fi	
 
-	for r in $(seq 1 1 14); do
+	for r in $(seq 1 1 20); do
 		
 		if [ ! -e "${WD}/${Subject}/run${r}/nswdktm_functional_6.nii.gz" ]; then
 			sed "s/s in P001/s in ${Subject}/g; s/run1/run${r}/g " < ${SCRIPTS}/proc_functional.sh > /home/despoB/kaihwang/tmp/proc_functional_${Subject}_${r}.sh 
