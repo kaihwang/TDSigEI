d151d507d7b217c3e4918b202035db61f735a50f
Lara Yang (via kaihwang) <lyang@nx3.priv>
2015-06-18 10:02:08 -0700

    changed scripts to work for qsub

--- organize_raw.sh
+++ organize_raw.sh
@@ -5,29 +5,27 @@ WD='/home/despoB/TRSEPPI/TDSigEI'
 
 cd ${WD}
 for subj in $(ls -d *); do
+	if [ "${subj}" != "Raw" ]; then
+		cd ${WD}/${subj}
 
-	cd ${WD}/${subj}
+		mprage_dir=$(ls -d MEMPRAGE_P2_RMS*)
 
-	mprage_dir=$(ls -d MEMPRAGE_P2_RMS*)
-
-	if [ ! -d ${WD}/${subj}/MPRAGE ]; then
-		ln -s ${mprage_dir} MPRAGE
-	fi	
+		if [ ! -d ${WD}/${subj}/MPRAGE ]; then
+			ln -s ${mprage_dir} MPRAGE
+		fi	
 	
-	BOLD_DIRs=($(ls -d tmsMRI_sequence_PA_TR1500_4mm_* | sort -V))
+		BOLD_DIRs=($(ls -d tmsMRI_sequence_PA_TR1500_4mm_* | sort -V))
 
-	for r in $(seq 1 24); do
-		i=$(($r-1))
-
-		if [ ! -d ${WD}/${subj}/run${r} ]; then
-			ln -s ${BOLD_DIRs[i]} run${r}
-		fi
-
-
-	done
+		for r in $(seq 1 24); do
+			i=$(($r-1))
 	
+			if [ ! -L  ${WD}/${subj}/run${r} ]; then
+			
+				ln -s ${BOLD_DIRs[i]} run${r}
+				
+			fi
 
-
-
+		done
+	fi
 
 done
--- proc_mprage.sh
+++ proc_mprage.sh
@@ -5,10 +5,10 @@
 
 WD='/home/despoB/TRSEPPI/TDSigEI'
 
-for s in $1; do
+for s in P001; do
 	cd ${WD}/${s}/MPRAGE
 
-	if [[ ! -e ${WD}/${s}/MPRAGE/mprage_final.nii.gz ]]; then
+	if [ ! -e ${WD}/${s}/MPRAGE/mprage_final.nii.gz ]; then
 		
 		preprocessMprage -r MNI_2mm -b "-R -f 0.46 -g -0.5" -d a -no_bias -o mprage_final.nii.gz -p "IM*"
 	fi
