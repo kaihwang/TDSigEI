192e86dca6c3e7819554a580229f28f970780683
kai.hwang@gmail.com <kaihwang@nx4.priv>
2015-07-10 10:42:48 -0700

    only include FP HP runs for mapping.

--- parse_stim.py
+++ parse_stim.py
@@ -8,33 +8,40 @@ import os.path
 
 #parse block order
 os.chdir('/home/despoB/kaihwang/TRSE/TDSigEI')
-#Subjects = glob.glob('5*')
-Subjects =[503, 505, 508, 509, 512, 513, 516, 517, 518, 523, 527]
+Subjects = glob.glob('5*')
+#Subjects =[503, 505, 508, 509, 512, 513, 516, 517, 518, 523, 527]
 
 os.chdir('/home/despoB/kaihwang/TRSE/TDSigEI/Scripts/Logs')
 
 for s in Subjects:
 
-	fn = '/home/despoB/TRSEPPI/TDSigEI/Scripts/%s_block_order' %s
+	fn = '/home/despoB/TRSEPPI/TDSigEI/Scripts/%s_run_order' %s
 	
 	filestring = 'fMRI_Data_%s*.txt' %s
 	timing_logs = glob.glob(filestring)
 
-	df = pd.read_table(timing_logs[0], header= None).append(pd.read_table(timing_logs[1], header= None)).append(pd.read_table(timing_logs[2],header= None)).append(pd.read_table(timing_logs[3], header= None))
+	df = pd.read_table(timing_logs[3], header= None).append(pd.read_table(timing_logs[2], header= None)).append(pd.read_table(timing_logs[1],header= None)).append(pd.read_table(timing_logs[0], header= None))
 	df.columns = ['SubjID', 'Condition', 'MotorMapping', 'Target', 'Accu', 'FA', 'RH', 'LH', 'RT', 'OnsetTime', 'pic']
 
 	df['Block'] = np.repeat(np.arange(1, 25),48)
 
+	run_order = df.groupby(['Block', 'Condition', 'MotorMapping']).sum().reset_index()[['Block', 'Condition', 'MotorMapping']]
 	if os.path.isfile(fn) is False:			
-		df.groupby(['Block', 'Condition', 'MotorMapping']).sum().reset_index()[['Condition','MotorMapping']].to_csv(fn, index=None, header=None, )
+		run_order[['Condition','MotorMapping']].to_csv(fn, index=None, header=None, )
 	
-	#create localizer regressors
-	Face_stimtime = [['*']*24]
-	House_stimtime = [['*']*24]
-	RH_stimtime = [['*']*24]
-	LH_stimtime = [['*']*24]
+	#write out mapping runs
+	mapping_runs = run_order[(run_order['Condition'] == 'Hp') | (run_order['Condition'] == 'Fp')]['Block'].values
+	fn = '/home/despoB/TRSEPPI/TDSigEI/Scripts/%s_mapping_runs' %s
+	if os.path.isfile(fn) is False:			
+		np.savetxt(fn, mapping_runs, fmt='%2d')
 
-	for block in np.arange(1,25):
+	#create localizer regressors
+	Face_stimtime = [['*']*8] #8 runs of FP HP 
+	House_stimtime = [['*']*8]
+	RH_stimtime = [['*']*8]
+	LH_stimtime = [['*']*8]
+
+	for i, block in enumerate(mapping_runs):
 		block_df = df[df['Block']==block].reset_index()
 		Face_block_trials = []
 		House_block_trials = []
@@ -43,29 +50,29 @@ for s in Subjects:
 		
 		for tr in np.arange(0, len(block_df)):
 			
-			if block_df.loc[tr,'Condition'] in ('Fp', 'Fo', 'FH'):
+			if block_df.loc[tr,'Condition'] in ('Fp'):
 				Face_block_trials.append(block_df.loc[tr,'OnsetTime'])
 
-			if block_df.loc[tr,'Condition'] in ('Hp', 'Ho', 'HF'):	
+			if block_df.loc[tr,'Condition'] in ('Hp'):	
 				House_block_trials.append(block_df.loc[tr,'OnsetTime'])
 
-			if block_df.loc[tr,'RH']:
-				RH_block_trials.append(block_df.loc[tr,'OnsetTime'] + block_df.loc[tr,'RT'] )						
+			if block_df.loc[tr,'RH'] and block_df.loc[tr,'Condition'] in ('Fp', 'Hp'):
+				RH_block_trials.append(block_df.loc[tr,'OnsetTime'])						
 			
-			if block_df.loc[tr,'LH']:
-				LH_block_trials.append(block_df.loc[tr,'OnsetTime'] + block_df.loc[tr,'RT'])
+			if block_df.loc[tr,'LH'] and block_df.loc[tr,'Condition'] in ('Fp', 'Hp'):
+				LH_block_trials.append(block_df.loc[tr,'OnsetTime'])
 
 		if any(Face_block_trials):
-			Face_stimtime[0][block-1] = Face_block_trials
+			Face_stimtime[0][i] = Face_block_trials
 		
 		if any(House_block_trials):
-			House_stimtime[0][block-1] = House_block_trials
+			House_stimtime[0][i] = House_block_trials
 		
 		if any(RH_block_trials):
-			RH_stimtime[0][block-1] = RH_block_trials
+			RH_stimtime[0][i] = RH_block_trials
 		
 		if any(LH_block_trials):	
-			LH_stimtime[0][block-1] = LH_block_trials
+			LH_stimtime[0][i] = LH_block_trials
 
 	fn = '/home/despoB/TRSEPPI/TDSigEI/Scripts/%s_face_stimtime.1D' %s
 	if os.path.isfile(fn) is False:
--- run_localizer.sh
+++ run_localizer.sh
@@ -4,62 +4,56 @@
 WD='/home/despoB/kaihwang/TRSE/TDSigEI'
 SCRIPTS='/home/despoB/kaihwang/TRSE/TDSigEI/Scripts'
 
-for s in 503; do
+for s in 505; do
 	cd ${WD}/${s}
 
 	ln -s /home/despoB/kaihwang/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_brain.nii \
 	template_brain.nii
 	
-	cat $(ls ${WD}/${s}/run*/motion.par | sort -V) > ${WD}/${s}/Motion_allruns.1D
+	#extract mapping runs
+	for run in $(cat ${SCRIPTS}/${s}_mapping_runs); do
+		
+		if [ ! -e ${WD}/${s}/Mapping_run${run}.nii.gz ]; then
+			ln -s ${WD}/${s}/run${run}/nswdktm_functional_6.nii.gz ${WD}/${s}/Mapping_run${run}.nii.gz
+		fi
+		
+		if [ ! -e ${WD}/${s}/Mapping_run${run}_motpar.1D ]; then
+			ln -s ${WD}/${s}/run${run}/motion.par ${WD}/${s}/Mapping_run${run}_motpar.1D
+		fi
 
-	3dDeconvolve -input $(ls run*/ns*functional_6.nii.gz | sort -V) \
-	-mask /home/despoB/kaihwang/TRSE/TDSigEI/ROIs/overlap.nii.gz \
+	done
+
+	cat $(ls ${WD}/${s}/Mapping_run*_motpar.1D | sort -V) > ${WD}/${s}/Motion_mapping_runs.1D
+
+	1d_tool.py -infile ${WD}/${s}/Motion_mapping_runs.1D \
+	-set_nruns 8 -show_censor_count -censor_motion 0.5 ${s}_mapping -censor_prev_TR -overwrite
+
+	3dDeconvolve -input $(ls ${WD}/${s}/Mapping_run*.nii.gz | sort -V) \
+	-mask ${WD}/ROIs/overlap.nii.gz \
 	-polort A \
-	-num_stimts 8 \
-	-stim_times 1 ${WD}/Scripts/${s}_face_stimtime.1D 'BLOCK5(1,1)' -stim_label 1 Faces \
-	-stim_times 2 ${WD}/Scripts/${s}_house_stimtime.1D 'BLOCK5(1,1)' -stim_label 2 Scenes \
-	-stim_file 3 ${WD}/${s}/Motion_allruns.1D[0] -stim_label 3 motpar1 -stim_base 3 \
-	-stim_file 4 ${WD}/${s}/Motion_allruns.1D[1] -stim_label 4 motpar2 -stim_base 4 \
-	-stim_file 5 ${WD}/${s}/Motion_allruns.1D[2] -stim_label 5 motpar3 -stim_base 5 \
-	-stim_file 6 ${WD}/${s}/Motion_allruns.1D[3] -stim_label 6 motpar4 -stim_base 6 \
-	-stim_file 7 ${WD}/${s}/Motion_allruns.1D[4] -stim_label 7 motpar5 -stim_base 7 \
-	-stim_file 8 ${WD}/${s}/Motion_allruns.1D[5] -stim_label 8 motpar6 -stim_base 8 \
-	-gltsym 'SYM: +1*Faces' -glt_label 1 Faces \
-	-gltsym 'SYM: +1*Scenes' -glt_label 2 Scenes \
-	-gltsym 'SYM: +1*Faces -1*Scenes' -glt_label 3 Faces-Scenes \
+	-num_stimts 10 \
+	-censor ${s}_mapping_censor.1D \
+	-stim_times 1 ${WD}/Scripts/${s}_face_stimtime.1D 'GAM' -stim_label 1 Faces \
+	-stim_times 2 ${WD}/Scripts/${s}_house_stimtime.1D 'GAM' -stim_label 2 Scenes \
+	-stim_times 3 ${WD}/Scripts/${s}_RH_stimtime.1D 'GAM' -stim_label 3 RH \
+	-stim_times 4 ${WD}/Scripts/${s}_LH_stimtime.1D 'GAM' -stim_label 4 LH \
+	-stim_file 5 ${WD}/${s}/Motion_mapping_runs.1D[0] -stim_label 5 motpar1 -stim_base 5 \
+	-stim_file 6 ${WD}/${s}/Motion_mapping_runs.1D[1] -stim_label 6 motpar2 -stim_base 6 \
+	-stim_file 7 ${WD}/${s}/Motion_mapping_runs.1D[2] -stim_label 7 motpar3 -stim_base 7 \
+	-stim_file 8 ${WD}/${s}/Motion_mapping_runs.1D[3] -stim_label 8 motpar4 -stim_base 8 \
+	-stim_file 9 ${WD}/${s}/Motion_mapping_runs.1D[4] -stim_label 9 motpar5 -stim_base 9 \
+	-stim_file 10 ${WD}/${s}/Motion_mapping_runs.1D[5] -stim_label 10 motpar6 -stim_base 10 \
+	-gltsym 'SYM: +1*Faces -1*Scenes' -glt_label 1 Faces-Scenes \
+	-gltsym 'SYM: +1*RH -1*LH' -glt_label 2 RH-LH \
 	-fout \
 	-rout \
 	-tout \
-	-bucket Localizer_FFAPPA_stats \
-	-x1D Localizer_FFAPPA_design_mat \
+	-bucket Localizer_stats \
+	-x1D Localizer_design_mat \
 	-GOFORIT \
 	-x1D_stop  
 
-	. Localizer_FFAPPA_stats.REML_cmd 
+	. Localizer_stats.REML_cmd 
 
-	3dDeconvolve -input $(ls run*/ns*functional_6.nii.gz | sort -V) \
-	-mask /home/despoB/kaihwang/TRSE/TDSigEI/ROIs/overlap.nii.gz \
-	-polort A \
-	-num_stimts 8 \
-	-stim_times 1 ${WD}/Scripts/${s}_LH_stimtime.1D 'BLOCK5(1,1)' -stim_label 1 LH \
-	-stim_times 2 ${WD}/Scripts/${s}_RH_stimtime.1D 'BLOCK5(1,1)' -stim_label 2 RH \
-	-stim_file 3 ${WD}/${s}/Motion_allruns.1D[0] -stim_label 3 motpar1 -stim_base 3 \
-	-stim_file 4 ${WD}/${s}/Motion_allruns.1D[1] -stim_label 4 motpar2 -stim_base 4 \
-	-stim_file 5 ${WD}/${s}/Motion_allruns.1D[2] -stim_label 5 motpar3 -stim_base 5 \
-	-stim_file 6 ${WD}/${s}/Motion_allruns.1D[3] -stim_label 6 motpar4 -stim_base 6 \
-	-stim_file 7 ${WD}/${s}/Motion_allruns.1D[4] -stim_label 7 motpar5 -stim_base 7 \
-	-stim_file 8 ${WD}/${s}/Motion_allruns.1D[5] -stim_label 8 motpar6 -stim_base 8 \
-	-gltsym 'SYM: +1*LH' -glt_label 1 LH \
-	-gltsym 'SYM: +1*RH' -glt_label 2 RH \
-	-gltsym 'SYM: +1*LH -1*RH' -glt_label 3 LH-RH \
-	-fout \
-	-rout \
-	-tout \
-	-bucket Localizer_Motor_stats \
-	-x1D Localizer_Motor_design_mat \
-	-GOFORIT \
-	-x1D_stop  
-
-	. Localizer_Motor_stats.REML_cmd 
 
 done
