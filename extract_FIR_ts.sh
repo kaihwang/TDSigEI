03877223d2d285cae8870a06c9d90432aa40216c
Kai Hwang <kai.hwang@gmail.com>
2015-08-13 21:41:17 -0700

    change to nifti.

--- extract_FIR_ts.sh
+++ extract_FIR_ts.sh
@@ -10,7 +10,7 @@ for s in $(ls -d 5*); do
 
 	for ROI in PPA FFA; do
 		for condition in FH Fo Fp HF Ho Hp; do
-			3dmaskave -mask ${ROI}_indiv_ROI+tlrc -q ${condition}_FIR+tlrc > ${WD}/FIR_1Ds/${s}_${ROI}_${condition}.1D
+			3dmaskave -mask ${ROI}_indiv_ROI.nii.gz -q ${condition}_FIR+tlrc > ${WD}/FIR_1Ds/${s}_${ROI}_${condition}.1D
 		done
 	done	
 
