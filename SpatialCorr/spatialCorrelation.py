494add0c4f88737e857557ccfe64087542041e42
Akshay Jagadeesh (via kaihwang) <akshayj@nx6.priv>
2015-12-06 20:02:23 -0800

    Updated FEF regression script. Uploading spatial correlation csv and graphs with and without FEF

--- SpatialCorr/spatialCorrelation.py
+++ SpatialCorr/spatialCorrelation.py
@@ -2,10 +2,12 @@ import os, numpy as np, nibabel as nib
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from scipy.stats.stats import pearsonr
+import pandas as pd
 
 dataPath = '/home/despoB/kaihwang/TRSE/TDSigEI/'
+dataPath2 = '/home/despoB/akshayj/TDSigEI/SpatialCorr/'
 subjects = ['503', '505', '508', '509', '510', '512', '513', '516', '517', '518', '519', '523', '527', '528', '529', '530', '531', '532', '534']
-
+#subjects = ['503']
 # blocks_to_keep = range(1,20) + range(28,47) + range(55,74) + range(82,101) \
 # + range(1+102,20+102) + range(28+102,47+102) + range(55+102,74+102) + range(82+102,101+102) \
 # + range(1+204,20+204) + range(28+204,47+204) + range(55+204,74+204) + range(82+204,101+204) \
@@ -16,32 +18,36 @@ repSuppArrFFA = np.zeros((len(subjects), 21))
 repEnhanceArrPPA = np.zeros((len(subjects), 21))
 repSuppArrPPA = np.zeros((len(subjects), 21))
 
+SpatialCorrDF = pd.DataFrame()
+corrFunc2 = lambda a, c, d: np.array(pearsonr(a, d)) - np.array(pearsonr(c, d))
 subjNum = 0
-for subj in subjects:
+for i, subj in enumerate(subjects):
 
-    os.chdir(dataPath+subj)
+    print '*******************{1}. SUBJECT {0}*******************'.format(subj, i)
+    SpatialCorrDF.set_value(i, 'Subject', subj)
+    os.chdir(dataPath2+subj)
     #convert to nifti
     files = os.listdir('.')
-    if 'FIR_Hp.nii.gz' not in files:
-        os.system("3dAFNItoNIFTI -prefix FIR_FH.nii.gz FH_FIR+tlrc")
-        os.system("3dAFNItoNIFTI -prefix FIR_HF.nii.gz HF_FIR+tlrc")
-        os.system("3dAFNItoNIFTI -prefix FIR_Fo.nii.gz Fo_FIR+tlrc")
-        os.system("3dAFNItoNIFTI -prefix FIR_Ho.nii.gz Ho_FIR+tlrc")
-        os.system("3dAFNItoNIFTI -prefix FIR_Fp.nii.gz Fp_FIR+tlrc")
-        os.system("3dAFNItoNIFTI -prefix FIR_Hp.nii.gz Hp_FIR+tlrc")
+    if 'FIR_Hp_T_FEFremoved.nii.gz' not in files:
+        os.system("3dAFNItoNIFTI -prefix FIR_FH_T_FEFremoved.nii.gz FH_FIR_T_FEFremoved+tlrc")
+        os.system("3dAFNItoNIFTI -prefix FIR_HF_T_FEFremoved.nii.gz HF_FIR_T_FEFremoved+tlrc")
+        os.system("3dAFNItoNIFTI -prefix FIR_Fo_T_FEFremoved.nii.gz Fo_FIR_T_FEFremoved+tlrc")
+        os.system("3dAFNItoNIFTI -prefix FIR_Ho_T_FEFremoved.nii.gz Ho_FIR_T_FEFremoved+tlrc")
+        os.system("3dAFNItoNIFTI -prefix FIR_Fp_T_FEFremoved.nii.gz Fp_FIR_T_FEFremoved+tlrc")
+        os.system("3dAFNItoNIFTI -prefix FIR_Hp_T_FEFremoved.nii.gz Hp_FIR_T_FEFremoved+tlrc")
     else:
         print('Skipping afni to nifti file conversion...')
-    fn = dataPath + subj + '/FIR_FH.nii.gz'
+    fn = 'FIR_FH_T_FEFremoved.nii.gz'
     FH_beta = nib.load(fn).get_data()
-    fn = dataPath + subj + '/FIR_HF.nii.gz'
+    fn = 'FIR_HF_T_FEFremoved.nii.gz'
     HF_beta = nib.load(fn).get_data()
-    fn = dataPath + subj + '/FIR_Fo.nii.gz'
+    fn = 'FIR_Fo_T_FEFremoved.nii.gz'
     Fo_beta = nib.load(fn).get_data()
-    fn = dataPath + subj + '/FIR_Ho.nii.gz'
+    fn = 'FIR_Ho_T_FEFremoved.nii.gz'
     Ho_beta = nib.load(fn).get_data()
-    fn = dataPath + subj + '/FIR_Fp.nii.gz'
+    fn = 'FIR_Fp_T_FEFremoved.nii.gz'
     Fp_beta = nib.load(fn).get_data()
-    fn = dataPath + subj + '/FIR_Hp.nii.gz'
+    fn = 'FIR_Hp_T_FEFremoved.nii.gz'
     Hp_beta = nib.load(fn).get_data()
 
     ffa = nib.load(dataPath + subj + '/FFA_indiv_ROI.nii.gz').get_data()
@@ -65,42 +71,14 @@ for subj in subjects:
     FH_ppaT = FH_beta[ppa!=0]
     HF_ppaT = HF_beta[ppa!=0]
 
-    # Measure 1: Representation selectivity
-    corrFuncFace = lambda c: np.array(pearsonr(c, Fo_ffa)) - np.array(pearsonr(c, Ho_ffa))
-    corrFuncHouse = lambda c: np.array(pearsonr(c, Ho_ppa)) - np.array(pearsonr(c, Fo_ppa))
-
-    print("\n******SUBJECT " + str(subj) + "******")
-    faceAttn = corrFuncFace(FH_ffa)
-    faceBaseline = corrFuncFace(Fp_ffa)
-    faceInhib = corrFuncFace(HF_ffa)
-    HouseAttn = corrFuncHouse(HF_ppa)
-    HouseBaseline = corrFuncHouse(Hp_ppa)
-    HouseInhib = corrFuncHouse(FH_ppa)
-    print ("Measure 1: Representation Selectivity")
-    print('\tFace Attn: {0}\n\t Face Baseline: {1}\n\t Face Inhibition {2}'.format(faceAttn[0], faceBaseline[0], faceInhib[0]))
-    print('\tHouse Attn: {0}\n\t House Baseline: {1}\n\t House Inhibition {2}'.format(HouseAttn[0], HouseBaseline[0], HouseInhib[0]))
-
-    # Measure 2a: Representation similarity compared to passive condition as template for FFA
-    corrFunc2 = lambda a, c, d: np.array(pearsonr(a, d)) - np.array(pearsonr(c, d))
-    repEnhancement = corrFunc2(FH_ffa, Fo_ffa, Fp_ffa)
-    repSuppression = corrFunc2(HF_ffa, Fo_ffa, Fp_ffa)
-    print('Measure 2a: FFA Representation Similarity compared to passive')
-    print('\tRepresentation Enhancement: {0}'.format(repEnhancement[0]))
-    print('\tRepresentation Suppression: {0}'.format(repSuppression[0]))
-
     #Measure 2b: Compared to target only condition as template for FFA
     repEnhancementB = corrFunc2(FH_ffa, Fp_ffa, Fo_ffa)
     repSuppressionB = corrFunc2(HF_ffa, Fp_ffa, Fo_ffa)
     print('Measure 2b: FFA Representation Similarity compared to attention only')
     print('\tRepresentation Enhancement: {0}'.format(repEnhancementB[0]))
     print('\tRepresentation Suppression: {0}'.format(repSuppressionB[0]))
-
-    #Measure 3a: Representation similarity compared to passive as template for PPA
-    repEnhancement = corrFunc2(HF_ppa, Ho_ppa, Hp_ppa)
-    repSuppression = corrFunc2(FH_ppa, Ho_ppa, Hp_ppa)
-    print('Measure 3a: PPA Representation Similarity compared to passive')
-    print('\tRepresentation Enhancement: {0}'.format(repEnhancement[0]))
-    print('\tRepresentation Suppression: {0}'.format(repSuppression[0]))
+    SpatialCorrDF.set_value(i, 'FFARepEnhancement', repEnhancementB[0])
+    SpatialCorrDF.set_value(i, 'FFARepSuppression', repSuppressionB[0])
 
     #Measure 3b: Compared to target only condition as template for PPA
     repEnhancementB = corrFunc2(HF_ppa, Hp_ppa, Ho_ppa)
@@ -108,6 +86,8 @@ for subj in subjects:
     print('Measure 3b: PPA Representation Similarity compared to attention only')
     print('\tRepresentation Enhancement: {0}'.format(repEnhancementB[0]))
     print('\tRepresentation Suppression: {0}'.format(repSuppressionB[0]))
+    SpatialCorrDF.set_value(i, 'PPARepEnhancement', repEnhancementB[0])
+    SpatialCorrDF.set_value(i, 'PPARepSuppression', repSuppressionB[0])
 
     # Measure 4a: Time series correlation coefficient for FFA
     repEnhanceArr1 = np.zeros(FH_ffaT.shape[1])
@@ -134,6 +114,7 @@ for subj in subjects:
     subjNum += 1
     #end for loop
 
+SpatialCorrDF.to_csv('/home/despoB/akshayj/TDSigEI/SpatialCorr/SpatialCorr_df.csv')
 # Average across subjects for PPA
 enhanceMeanPPA = repEnhanceArrPPA.mean(0)
 enhanceErrorPPA = repEnhanceArrPPA.std(0) / np.sqrt(len(subjects))
