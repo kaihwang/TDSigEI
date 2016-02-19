3082d974b5b050aa46923ce8cdf990fb1cef53bd
Kai Hwang <kai.hwang@gmail.com>
2016-02-19 13:35:05 -0800

    plot new FIRs.

--- MTD/sge_submit.sh
+++ MTD/sge_submit.sh
@@ -8,7 +8,7 @@ SCRIPTS='/home/despoB/kaihwang/bin/TDSigEI'
 
 cd ${WD}
 
-for Subject in $(/bin/ls -d 5*); do
+for Subject in 508 510 513 517 519 527 529 531 534 537 540 546 549 505 509 512 516 518 523 528 530 532 536 539 542 547 550; do #$(/bin/ls -d 5*)
 
 	sed "s/s in 503/s in ${Subject}/g" < ${SCRIPTS}/MTD/run_MTD_reg_model.sh> ~/tmp/MTDmodel_${Subject}.sh
 	qsub -l mem_free=15G -V -M kaihwang -m e -e ~/tmp -o ~/tmp ~/tmp/MTDmodel_${Subject}.sh
